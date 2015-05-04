0edcc67d82fe3260d1c6a5e22cd630417e5a475c
joaofelixjesus <joaofelixjesus@gmail.com>
2015-05-04 11:30:17 +0100

    Criei o layout da GameActivity

--- QuemQuerSerMilionario/app/src/main/java/android/flag/joaojesus/quemquersermilionario/GameActivity.java
+++ QuemQuerSerMilionario/app/src/main/java/android/flag/joaojesus/quemquersermilionario/GameActivity.java
@@ -2,45 +2,18 @@ package android.flag.joaojesus.quemquersermilionario;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    private final static String LOG_TAG = "LOG_TAG";
+public class GameActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Log.d(LOG_TAG, "onCreate");
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
 
-        /**
-         *
-         * This line "connects" the layout with this activity.
-         *
-         */
 
-        setContentView(R.layout.activity_main);
-
-        /**
-         *
-         * Get the button and set the listener
-         *
-         * */
-
-        Button btn_MainActivity = (Button) findViewById(R.id.btn_MainActivity);
-        btn_MainActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
 
@@ -61,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
     }
 
